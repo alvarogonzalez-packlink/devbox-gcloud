759c61ed6db69d37916e5125b2d90cc2124dc7da
mohsenari <mohsen.ansari@jetpack.io>
2023-06-06 13:47:12 -0400

    fixed flake

--- flake.nix
+++ flake.nix
@@ -15,20 +15,11 @@
   }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = (import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-        });
-        nixpkgs-8670e4-pkgs = (import nixpkgs-8670e4 {
-          inherit system;
-          config.allowUnfree = true;
-        });
+        pkgs = nixpkgs.legacyPackages.${system};
       in
       {
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            (nixpkgs-8670e4-pkgs.google-cloud-sdk.withExtraComponents [google-cloud-sdk.components.gke-gcloud-auth-plugin])
-          ];
+        packages = {
+          gce = pkgs.google-cloud-sdk.withExtraComponents ([pkgs.google-cloud-sdk.components.gke-gcloud-auth-plugin]);
         };
       });
 }
